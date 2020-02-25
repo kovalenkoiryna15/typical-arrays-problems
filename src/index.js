8b2bd23b19692587ebaab4e0595d21a55606eb2a
kovalenkoiryna15 <52598991+kovalenkoiryna15@users.noreply.github.com>
2020-02-25 20:39:34 +0300

    init: submit task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (typeof array !== "undefined" && array.length !== 0) {
+    return array.sort(function(a,b){ 
+      return a - b
+    }).shift();
+  } else {
+    return 0;
+  };  
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (typeof array !== "undefined" && array.length !== 0) {
+    return array.sort(function(a,b){ 
+      return a - b
+    }).pop();
+  } else {
+    return 0;
+  };
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (typeof array !== "undefined" && array.length !== 0) {
+    return array.reduce((sum, current) => sum + current, 0) / array.length;
+  } else {
+    return 0;
+  };
+};
